17fa40635b8877db3fdcf222d97be86ca0a41d69
Phillip Mitchell <4kphil.@Phillips-MacBook-Air.local>
2023-05-14 11:02:07 -0400

    added links and routes from in class activity

--- src/App.js
+++ src/App.js
@@ -10,22 +10,33 @@ import Product from './components/products/product'
 import lamps from './data/lamps.json'
 import candles from './data/candles.json'
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Header />
       <div className="wrapper">
       <Nav />
       <main>
-        <Home />
-        <About />
-        <Newsletter />
-        <ProductList products={lamps} type={'Lamps'}/>
-        <ProductList products={candles} type={'Candles'}/>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/newsletter" element={<Newsletter />} />
+            <Route
+              path="/lamps"
+              element={<ProductList products={lamps} type={"Lamps"} />}/>
+            <Route
+              path="/candles"
+              element={<ProductList products={candles} type={"Candles"} />}/>
+        </Routes>
       </main>
     </div>
     <Footer />
+    </Router>
     </div>
   );
 }
--- src/components/common/nav.js
+++ src/components/common/nav.js
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Nav() {
   return (
-      <nav> 
-      <div>
-
+    <nav>
+    <div>
+      <Link to="/lamps">
         <h3>Lamps</h3>
-
+      </Link>
   
-
+      <Link to="/candles">
         <h3>Candles</h3>
- 
-      </div>
-    </nav>
+      </Link>
+    </div>
+  </nav>
   );
 };
